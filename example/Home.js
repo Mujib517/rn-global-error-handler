8ba995a13c314f299494142a341cc243fc90223f
Mujib <mujib517@gmail.com>
2020-02-18 16:41:13 +0530

    Add example react native project to do quick testing

---
+++ example/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Button } from "react-native";
+
+export default () => {
+    const onBtnPress = () => {
+
+    };
+
+    return <Button onPress={onBtnPress} title="Click Me"></Button>
+}
