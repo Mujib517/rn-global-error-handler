ef718a37a81ecb1b0607d9f692db1e56ea6ae3de
Mujib <mujib517@gmail.com>
2020-03-10 17:28:21 +0530

    Test package locally

--- example/Home.js
+++ example/Home.js
@@ -3,7 +3,7 @@ import { Button } from "react-native";
 
 export default () => {
     const onBtnPress = () => {
-
+        throw new Error("Failed!");
     };
 
     return <Button onPress={onBtnPress} title="Click Me"></Button>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SafeComponent extends Component {
   constructor() {
